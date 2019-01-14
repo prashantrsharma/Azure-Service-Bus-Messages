56b0e4525e8de7db3f7380c284675e254385a2df
Sharma, Prashant <prashant.r.sharma@capgemini.com>
2019-01-14 13:36:41 +0530

    Added to send,receive messages.

--- src/WorkingWithMessages.Receiver/Program.cs
+++ src/WorkingWithMessages.Receiver/Program.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using WorkingWithMessages.Config;
 
 namespace WorkingWithMessages.Receiver
 {
     class Program
     {
+        static readonly QueueClient Client = QueueClient.CreateFromConnectionString(Settings.ConnectionString,Settings.QueueName);
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Receiver Console - Hit Enter");
+            Console.ReadLine();
+            Console.WriteLine("Receiver Console - Is ready to receive new messages.....");
+
+            //Receive and Process the message
+            ReceiveAndProcess();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Receiver Console - Complete");
+            Console.ReadLine();
+        }
+
+        static void ReceiveAndProcess()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Client.OnMessage(message =>
+            {
+                Console.WriteLine($"Received:{message.Label}");
+            },new OnMessageOptions{AutoComplete = true});
         }
     }
 }
--- src/WorkingWithMessages.Sender/Program.cs
+++ src/WorkingWithMessages.Sender/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using WorkingWithMessages.Config;
 
 namespace WorkingWithMessages.Sender
 {
@@ -10,6 +14,62 @@ namespace WorkingWithMessages.Sender
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sender Console - Hit Enter");
+            Console.ReadLine();
+            Console.WriteLine("Sender Console - Is ready to send new messages.....");
+
+            Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //Send Text String
+            SendTextString(Settings.TextString, false);
+            //}
+
+            watch.Stop();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Sender Console - Complete");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($".........It took {watch.Elapsed.TotalMinutes} minutes to process the current transaction.........");
+            Console.ReadKey();
+
+        }
+
+        static void SendTextString(string text, bool sendSync)
+        {
+            //Create a Client
+            QueueClient queueClient = QueueClient.CreateFromConnectionString(Settings.ConnectionString, Settings.QueueName);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Sending:");
+            foreach (char letter in text.ToCharArray())
+            {
+                //Create an Empty Message and Set the Label
+                BrokeredMessage message = new BrokeredMessage
+                {
+                    Label = letter.ToString()
+                };
+
+                if (sendSync)
+                {
+                    //Send the Message
+                    queueClient.Send(message);
+                    Console.WriteLine(message.Label);
+                }
+                else
+                {
+                    Task.Run(async () =>
+                    {
+                        await queueClient.SendAsync(message);
+                        Console.WriteLine(message.Label);
+                    });
+                }
+            }
+
+            // Console.ReadLine();
+            Console.WriteLine();
+
+            //Always close the client
+            queueClient.Close();
         }
     }
 }
